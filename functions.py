0ef4f7f3c42b4b15996728449d5a8818967879d0
vfmas15 <vicente.feced.mas@gmail.com>
2023-02-20 21:19:32 +0100

    Functions updated

--- functions.py
+++ functions.py
@@ -1,24 +1,14 @@
 import torch
 
-class GaussFunction(torch.nn.Module):
-  def __init__(self, mean, std):
-    super(GaussFunction, self).__init__()
+def gauss(mean, std, x):
+  mean = - mean
+  x = x + mean
+  x = x ** 2
+  x = - x
 
-    self.mean = mean
-    self.std = std
-
-  def forward(self, x):
-    mean = self.mean
-    std = self.std
-    
-    mean = - mean
-    x = x + mean
-    x = x ** 2
-    x = - x
-
-    std = std ** 2
-    std = 2 * std
-    std = 1 / std
-    x = x * std
-    x = torch.exp(x)
-    return x
+  std = std ** 2
+  std = 2 * std
+  std = 1 / std
+  x = x * std
+  x = torch.exp(x)
+  return x
